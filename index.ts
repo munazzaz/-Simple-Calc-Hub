5476e014e5dc56562362c91f8b6c4654743e30d7
Munazza Zahid <128850097+munazzaz@users.noreply.github.com>
2023-08-29 00:25:58 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,36 @@
+import inquirer from "inquirer";
+
+async function main() {
+    let { firstNumber, secondNumber, operation } = await inquirer.prompt([
+        {
+            message: "Enter first Number",
+            type: "number",
+            name: "firstNumber"
+        },
+        {
+            message: "Enter second Number",
+            type: "number",
+            name: "secondNumber"
+        },
+        {
+            name: "operation",
+            type: "list",
+            choices: ["addition", "subtraction", "multiplication", "division"],
+            message: "Enter your operation"
+        }
+    ]);
+
+    if (operation === "addition") {
+        console.log(`${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber}.`);
+    } else if (operation === "subtraction") {
+        console.log(`${firstNumber} - ${secondNumber} = ${firstNumber - secondNumber}.`);
+    } else if (operation === "multiplication") {
+        console.log(`${firstNumber} x ${secondNumber} = ${firstNumber * secondNumber}.`);
+    } else if (operation === "division") {
+        console.log(`${firstNumber} / ${secondNumber} =  ${firstNumber / secondNumber}.`);
+    } else {
+        console.log("The operation is invalid.");
+    }
+}
+
+main();
